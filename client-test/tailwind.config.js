840287f183c6a57df21930027342172ed942a77f
Shardul Dhekane <shardulmd@gmail.com>
2025-03-20 21:51:16 +0530

    /

--- client-test/tailwind.config.js
+++ client-test/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: '2rem',
-      },
-    },
+    extend: {},
   },
+  darkMode: 'class',
   plugins: [],
-};
+}
